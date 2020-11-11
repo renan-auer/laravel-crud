fe0f07522d5fb574511dc7d08809486cfc99a0ca
Renan <renan@girafacomunicacao.com.br>
2020-11-11 09:12:54 -0300

    list clients

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;   
+use App\Cliente;
+
+class ClienteController extends Controller
+{
+
+    public function index(){
+        $clientes = Cliente::paginate(10);
+        return view('cliente.index', compact('clientes'));
+    }
+
+}
---
+++ database/migrations/2020_11_09_212129_create_clientes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->text('nome');
+            $table->text('cpfCnpj');
+            $table->timestamps();
+        });
+    }
+
+      
+
+    
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
